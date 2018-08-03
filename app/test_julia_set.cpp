cb70ef656af7e8a319823ab219a3032e9d04fd42
Tania Akter <tania9309@gmail.com>
2018-08-03 06:06:07 -0700

    Implementation of julia set, gives completely wrong output

--- app/test_julia_set.cpp
+++ app/test_julia_set.cpp
@@ -1,6 +1,55 @@
 #include "ra/julia_set.hpp"
+#include <string>
+#include <complex>
+#include "boost/multi_array.hpp"
+#include <chrono>
+#include <iostream>
+
+std::string make_plot(boost::multi_array<int, 2> a)
+{
+      std::string str;
+      std::size_t H = a.shape()[0];
+      std::size_t W = a.shape()[1];
+    
+      str.push_back('P');
+      str.push_back('2');
+      str.append(" ");
+      str.append(std::to_string(W));
+      str.append(" ");
+      str.append(std::to_string(H));
+      str.append(" ");
+      str.append(std::to_string(255));
+      str.push_back('\n');
+      
+
+      for(std::size_t i = 0; i< H; ++i)
+      {
+          for(std::size_t j = 0 ; j < W; ++j)
+          {
+              str.append(std::to_string( a[i][j] ));
+              str.append(" ");              
+          }
+          str.push_back('\n');
+      }
+      return str;
+}
+
 
 int main()
 {
-    return 0;
+
+  std::complex<float> bottom_left(-1.25, -1.25);
+  std::complex<float> top_right(1.25, 1.25);
+  boost::multi_array <int, 2> a(boost::extents [512][512] );
+  int max_iters = 255;
+  std::complex<float> c(0.37, -0.16);
+  
+  ra::fractal::compute_julia_set (bottom_left, top_right, c, max_iters, a, 1);
+  
+  std::cout << make_plot(a);
+  // auto z = std::complex<float>(0,0);
+  // auto c = std::complex<float>(1,1);
+  
+  // std::cout << ra::fractal::y_m<float>(255, z, c) << std::endl;
+  return 0;
 }
--- include/ra/julia_set.hpp
+++ include/ra/julia_set.hpp
@@ -1,43 +1,127 @@
 #include <complex>
 #include "boost/multi_array.hpp"
 #include <chrono>
+#include <string>
+#include <iostream>
+#include "ra/thread_pool.hpp"
 
 namespace ra :: fractal {
-template <class Real>
-void compute_julia_set (const std :: complex <Real>& bottom_left ,
-                        const std::complex <Real>& top_right,
-                        const std::complex <Real>& c ,
-                        int max_iters , 
-                        boost::multi_array <int, 2>& a,
-                        int num_threads )
-{
 
+
+template <class Real>
+std::complex<Real>  f_z(const std::complex <Real> z, const std::complex <Real>& c)
+{
+  // auto k = std::pow(z,2) + c;
+  // std::cout << "In fz : " << "re: " << k.real() << " img: " << k.imag() << std::endl; 
+  return (z*z) + c;
 }
 
 template <class Real>
-void calculate f_z(const std::complex <Real>& c, int i)
+std::complex<Real>  z_n(int n, std::complex<Real> z, const std::complex<Real>& c)
 {
 
+  if(n == 0)
+  {
+    return z;
+  }
+  z = f_z(z_n(n-1,z,c), c);
+
+  return z;
 }
+
 
 template <class Real>
-void calculate f_z()
+int n_z(int m, std::complex <Real> z, const std::complex <Real>& c)
 {
-
+  for(int i=0; i< m; ++i)
+  {
+    // auto ch = z_n(i, z, c);
+    // std::cout << "VALUE GOTTEN BACK AT n_z " << ch.real() << " " << ch.imag() << " " << "i: " << i << std::endl;
+    // std::cout << std::abs(ch) << std::endl;  
+    if(std::abs(z_n(i, z, c)) > (Real) 2)
+    {
+      return i;
+    }
+  }
+  //did not find i that gives 2, so return the max we can get
+  return std::numeric_limits<int>::max();
 }
+
 
 template <class Real>
-void calculate f_z()
+int  y_m(int m, std::complex <Real> z, const std::complex <Real>& c)
 {
-
+  return std::min(n_z(m, z, c), m);
 }
+
 
 template <class Real>
-void calculate f_z()
+std::complex<Real> get_z(Real u_0, Real u_1, Real v_0, Real v_1, std::size_t l, std::size_t k, std::size_t H, std::size_t W)
 {
-
+  Real re = u_0 + (k/(W-1))*(v_0 - u_0);
+  Real im = u_1 + (l/(H-1))*(v_1 - u_1);
+  
+  // Real re = k;
+  // Real im = l;
+  
+  std::complex <Real> r(re,im);
+  // std::cout << u_0 << " ";
+  // std::cout << u_1 << " ";
+  // std::cout << v_0 << " ";
+  // std::cout << v_1 << " ";
+  // std::cout << l << " ";
+  // std::cout << k << " ";
+  // std::cout << H << " ";
+  // std::cout << W << " ";
+  // std::cout << re << " ";
+  // std::cout << im << std::endl;
+  return r;
 }
 
+  template <class Real>
+  void compute_julia_set (const std::complex <Real>& bottom_left ,
+                          const std::complex <Real>& top_right,
+                          const std::complex <Real>& c ,
+                          int max_iters , //m
+                          boost::multi_array <int, 2>& a,
+                          int num_threads )
+  {
+    ra::concurrency::thread_pool tp2(num_threads);
+    std::mutex  mcout;
+    std::size_t H = a.shape()[0];
+    std::size_t W = a.shape()[1];
+    //get the row -> height H
+    for (std::size_t i = 0; i < H; ++i)
+    {
+      tp2.schedule([i, &mcout, bottom_left, top_right, H, W, max_iters, c, &a]() {
+        
+        //delay
+        // for (int j = 0; j < 100; ++j);
+        mcout.lock();
+        //for each column in the row -> width H
+        for (std::size_t k = 0; k < W; ++k)
+        {
+          //get z for each point
+          std::complex <Real> z = get_z(bottom_left.real(), 
+                                              bottom_left.imag(), 
+                                              top_right.real(), 
+                                              top_right.imag(),
+                                              i,
+                                              k,
+                                              H,
+                                              W
+                                              );
 
+          a[i][k] = y_m(max_iters,z,c);
+          
+          // std::cout << a[i][k] << std::endl;
+        }
+
+        mcout.unlock();
+      });
+    }
+
+    tp2.shutdown();
+  }
 
 }
